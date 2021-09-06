4be01203ab9bf8762a3730e7dcc938e28758d380
tonglingdyq <tonglingdyq@hotmail.com>
2021-09-06 16:04:01 +0800

    Update baseline_labmachine.py
    Minor updates for clarification - Same as last commit

--- baseline_labmachine.py
+++ baseline_labmachine.py
@@ -2,66 +2,86 @@ import numpy as np
 import pandas as pd
 from math import *
 from datetime import date
-import func, collections, utils, importlib, ujson, pathlib, os
+import func, collections, utils, importlib, ujson, pathlib, os, time
 
 ###################### Input #######################
-
 # Input data file
 current_path = pathlib.Path(os.getcwd()) #Get the current working directory
 raw_trip_file = 'trippub_top2k.csv' #File name of the 2k data
 trip_ls  = func.trip_ls_input(raw_trip_file,'w') #Generate the day trips for dataset
 
-#Create output folder
+# Create output folder
 output_path = str(current_path.parent)+ '/Results/Baseline_LabMachine/'+str(date.today()) #Output file path
-if not os.path.exists(output_path): #Create output folder if not exists
-    os.makedirs(output_path)
+pathlib.Path(output_path).mkdir(parents=True, exist_ok=True) #Create the folder (and parent folder) if not exists yet 
 
-#Read raw distance dictionary
-f = open('output/dist_dict0.json','r') #Required input for this file - distance dictionary to reduce computation complexity
-dist_dict0 = ujson.loads(f.read()) #Load the file
-dist_dict0 = utils.dict_key2tuple(dist_dict0) #Convert the file to tuples (original format before saving to .json)
+# Raw distance dictionary
+dist_dict_file_path = 'dist_dict.json' #File path for writing/reading
+######## Compute and save distances between raw journeys (commented) ########
+dist_dict0 = utils.cal_cross_dist(trip_ls, trip_ls)  #Compute the distances
+# utils.dict2json(dist_dict_file_path, dist_dict0) #Save the distances
+######## Read and load distances between raw journeys ########
+# dist_dict0 = utils.json2dict(dist_dict_file_path)[0] #existing distance dictionary 
+# dist_dict0 = utils.dict_key2tuple(dist_dict0) #Convert the file to tuples (original format before saving to .json)
 
 ###################### Input #######################
 #Generate initial population (a set of CJMs each as an individual)
 m = 10 # Maximum length of CJs in initial ppl CJMs (also determines the number of ini ppl CJMs = m-1)
 n = 5 # Number of CJs in initial ppl CJMs
-gen_max = 50 #Number of generations
-file_no_tot = 20 #Number of trials (each trial produces a separate output file)
-###################### New #######################
-for file_no in range(file_no_tot):
-	ppl, top_n = func.ini_ppl_gen(trip_ls, m, n) #Generate the initial populations
-	center_dict_all = {} #Create an empty dictionary for assignment
-	cjm_score = collections.defaultdict(list)
+gen_max = 100 #Number of generations
+trial_no_tot = 1 #Number of trials (each trial produces a separate output file)
+###################### Main Loop #######################
+start_time = time.time()
+# The purpose of the following loop is to generate populations (i.e., cluster centers) that have the highest score
+for trial_no in range(trial_no_tot):
+	last_time = time.time()
+	# Iterate over different number of trials to avoid local minimum
+	ppl, top_n = func.ini_ppl_gen(trip_ls, m, n) #Generate the initial populations for cluster centers
+	center_dict_all = {} #Initialize the dictionary for assignment
+	cjm_score = collections.defaultdict(list) #Initialize the score dictionary
 	record = [['Key','Best CJM','Score']] #List of [best CJM key, current best CJM, current best score]
-	filename= output_path + '/FinalResult' + str(file_no+1) + '.csv' #Create name of the output folder
-	func.save_ls2csv(record, 'w' , filename) #Saves the header 'record'
+	filename= output_path + '/FinalResult_Trial_#' + str(trial_no+1) + '.csv' #Create name of the output folder
+	func.save_ls2csv(record, 'w' , filename) #Saves the header: 'record'
 
 	for t in range(gen_max):
-		#Assign CJs to current ppl and evaluate current population (CJMs)
-		for key, center_ls in ppl.items(): #key is identifier in [2, top_num], center_ls is a CJM (a list of centers/CJs in the CJM)
-			#Note that for initial ppl, key is length of individual journey (i in CJM_i).
+		#Assign CJs to current population and evaluate current population's score (CJMs)
+		for key, center_ls in ppl.items(): 
+			# Iterate over each CJM in testing
+			# Note:
+				# Center_ls is a CJM (a list of centers/CJs in the CJM)
+				# key is a numerical identifier that belong to [2, top_num]
+					# For initial ppl, key is length of individual journey (i in CJM_i).
 			dist_dict1 = utils.cal_cross_dist(trip_ls, center_ls, dist_dict0) #Compute the distances between all trips and current centers given dist_dict0
-			center_dict_all[key] = func.cjm_assign(trip_ls, center_ls, dist_dict1) #Assign each journey to the closest centers/cluster in CJM_i (returns a dictionary)
+			#Assign each journey to the closest centers/cluster in CJM_i (returns a dictionary - see details in cjm_assign function)
+			center_dict_all[key] = func.cjm_assign(trip_ls, center_ls, dist_dict1) # Each item of center_dict_all is a dict for ONE CJM
+
 			score = func.cjm_eval(trip_ls, center_ls, center_dict_all[key], dist_dict1) #Compute score for the current CJM
 			cjm_score[key].append(score) #Saves the score for current CJM and generation (each cjm_score[key] is a list of scores of history)
-		cjm_sort_ls = sorted(cjm_score, key = lambda x: cjm_score[x][-1], reverse = 1) #Sort the CJM by their latest score in descending order
+		
+		# Sort the CJM by their latest score in descending order
+		cjm_sort_ls = sorted(cjm_score, key = lambda x: cjm_score[x][-1], reverse = 1) 
 		print('The best CJM at time ' + str(t) + ' is CJM #' + str(cjm_sort_ls[0]) +' and its score is '+ str(cjm_score[cjm_sort_ls[0]][t]))
-		#Saves [key, current best CJM, current best score] to a list
-		record = [str(cjm_sort_ls[0]), str(ppl[cjm_sort_ls[0]]), str(cjm_score[cjm_sort_ls[0]][t])]
-		func.save_ls2csv(record,'a' , filename)
-		ppl = func.ga_CJM(ppl, cjm_score, top_n) #Genetic operations
 
-	# Final evaluation
+		# Saves [key, current best CJM, current best score] for current trial to csv
+		record = [str(cjm_sort_ls[0]), str(ppl[cjm_sort_ls[0]]), str(cjm_score[cjm_sort_ls[0]][t])]
+		func.save_ls2csv(record,'a' , filename) 
+
+		# Performs genetic operations
+		ppl = func.ga_CJM(ppl, cjm_score, top_n) 
+
+	# Evaluation for the last round of genetic operations
 	for key, center_ls in ppl.items(): #center_ls is a CJM (a list of centers/CJs in the CJM), key is identifier in [2, top_num]
-		dist_dict1 = utils.cal_cross_dist(trip_ls, center_ls, dist_dict0) #Compute the distances between all trips and current centers
+		dist_dict1 = utils.cal_cross_dist(trip_ls, center_ls, dist_dict1) #Compute the distances between all trips and current centers
 		center_dict_all[key] = func.cjm_assign(trip_ls, center_ls, dist_dict1) #Assign each journey to the closest centers/cluster in CJM_i
 		score = func.cjm_eval(trip_ls, center_ls, center_dict_all[key], dist_dict1) #Compute score for the current CJM
 		cjm_score[key].append(score)
 	cjm_sort_ls = sorted(cjm_score, key = lambda x: cjm_score[x][-1], reverse = 1) #Sort the CJM by their latest score in descending order
-	print('The best CJM at time ' + str(t) + ' of trial '+ str(file_no)+ ' is CJM #' + str(cjm_sort_ls[0]) +' and its score is '+ str(cjm_score[cjm_sort_ls[0]][t]))
+	print('The best CJM at time ' + str(t) + ' of trial '+ str(trial_no)+ ' is CJM #' + str(cjm_sort_ls[0]) +' and its score is '+ str(cjm_score[cjm_sort_ls[0]][t]))
 	record = [str(cjm_sort_ls[0]), str(ppl[cjm_sort_ls[0]]), str(cjm_score[cjm_sort_ls[0]][t])]
-	func.save_ls2csv(record, 'a' , filename)
-	print('The final CJM of trial '+ str(file_no)+ ' is #'+ str(cjm_sort_ls[0]) +': '+str(ppl[cjm_sort_ls[0]]))
+	func.save_ls2csv(record, 'a' , filename) #Saves to csv
+
+	# Final Printings
+	print('The final CJM of trial '+ str(trial_no)+ ' is #'+ str(cjm_sort_ls[0]) +': '+str(ppl[cjm_sort_ls[0]]))
+	print('Execution time of this trial is',time.time()-last_time,'and current total time taken is',time.time()-start_time)
 
 ###################### Test #######################
 # importlib.reload(func)

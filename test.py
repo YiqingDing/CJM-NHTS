70a383a3842690a82598a3bc5a2e7bafa30f4641
tonglingdyq <tonglingdyq@hotmail.com>
2021-10-07 15:31:19 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,24 @@
-import openpyxl, pathlib, os, sys
-import pandas as pd
 import numpy as np
+import pandas as pd
+from math import *
+from datetime import date
+import func, collections, utils, importlib, ujson, pathlib, os
 
-workbook_path = str(pathlib.Path(os.getcwd()).parent)+'/Results/Bayesian/test.xlsx'
-folder_path = os.path.split(workbook_path)[0] #Extract the folder path for the file
-pathlib.Path(folder_path).mkdir(parents=True, exist_ok=True) #Create the folder (and parent folder) if not exists yet 
-if os.path.exists(workbook_path): #Remove file if it already exists
-	os.remove(workbook_path)
+###################### Input #######################
 
-workbook = openpyxl.Workbook()
-ws0 = workbook.active
-ws0.title = 'General Results'
-ws0.cell(row = 2, column = 1).value = 'Hello world'
-workbook.save(workbook_path)
-x = np.asarray([[1,2],[3,4]])
-current_title = [x,np.full((2,1),None),x+1]
-current_title_row = np.concatenate(current_title,axis = 1)
-for i in range(3):
-	workbook = openpyxl.load_workbook(workbook_path)
-	ws1 = workbook.create_sheet('mY'+str(i))
-	for row in current_title_row:
-		# print(row)
-		ws1.append(row.tolist())
-	# for col_i, cell_val in enumerate(current_title, start = 1):
-	# 	ws1.cell(row = 1, column=col_i).value = cell_val
-	workbook.save(workbook_path)
+# Input data file
+current_path = pathlib.Path(os.getcwd()) #Get the current working directory
+raw_trip_file = 'trippub_top2k.csv' #File name of the 2k data
+trip_ls  = func.trip_ls_input(raw_trip_file,'w') #Generate the day trips for dataset
+
+# # Create output folder
+# output_path = str(current_path.parent)+ '/Results/Baseline_LabMachine/'+str(date.today()) #Output file path
+# pathlib.Path(output_path).mkdir(parents=True, exist_ok=True) #Create the folder (and parent folder) if not exists yet 
+
+# Raw distance dictionary
+######## Compute and save distances between raw journeys ########
+dist_dict_file_path = 'dist_dict_baseline.json'
+dist_dict0 = utils.cal_cross_dist(trip_ls, trip_ls)
+utils.dict2json(dist_dict_file_path, dist_dict0)
+
+# utils.json2dict(dist_dict_file_path)[0]
